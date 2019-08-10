61ecf30ed63e1bc1ad984c044d714fe452c5fd0c
Joe Dukes <joedukes.email@gmail.com>
2019-08-10 11:33:24 -0400

    update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
-export default () => <Typography variant='display1'>Welcome Home!</Typography>;
+export default () => (
+  <Typography variant='display1'>
+    This is the single page app that's going to get me a job offer from
+    Code/+/Trust
+  </Typography>
+);
